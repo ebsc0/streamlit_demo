42b04e263de989d7c0cfc2afee49841d939c8cf9
eugenebaek <eugenebaek5100@gmail.com>
2024-08-24 23:31:17 -0700

    day 1-13

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # streamlit_demo
+
+30 days of streamlit: <https://30days.streamlit.app/>
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import numpy as np
 import altair as alt
 import pandas as pd
+from datetime import time, datetime
 import streamlit as st
 
-st.write('Hello World!')
+st.write('Day 1-13')
 
 # st.write 
 st.header('st.write')
@@ -32,3 +33,68 @@ if st.button('say hello'):
     st.write('hello there')
 else:
     st.write('goodbye')
+
+# st.slider
+st.header('st.slider')
+
+st.subheader('Slider')
+age = st.slider('how old are you?', 0, 130, 25) # args: string, max, min, default
+st.write('im ', age, ' years old')
+
+st.subheader('Range slider')
+values = st.slider(
+    'select range of values',
+    0.0, 100.0, (25.0, 75.0) # tuple denotes default value pair
+)
+st.write('values: ', values)
+
+st.subheader('Range time slider')
+appointment = st.slider(
+    'schedule your appointment:',
+    value=(time(11,30), time(12,45)) # tuple denotes default datetime value pair
+)
+st.write('youre scheduled for: ', appointment)
+
+st.subheader('Datetime slider')
+start_time = st.slider(
+    'when do you start?',
+    value=datetime(2020,1,1,9,30), # default datetime value
+    format='MM/DD/YY - hh:mm'
+)
+st.write('start time: ', start_time)
+
+st.header('st.line_chart') # based on altair charts
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['a','b','c']
+)
+st.line_chart(chart_data)
+
+st.header('st.selectbox')
+option = st.selectbox(
+    'what is your favorite color?',
+    ('blue', 'green', 'red')
+)
+st.write('your favorite color is ', option)
+
+st.header('st.multiselect')
+options = st.multiselect(
+    'what are your favorite colors?',
+    ['green','yellow','red','blue'],
+    ['yellow','red'] # default selection
+)
+st.write('you selected: ', options)
+
+st.header('st.checkbox')
+st.write('what would you like to order?')
+
+ice_cream = st.checkbox('ice cream')
+coffee = st.checkbox('coffee')
+cola = st.checkbox('cola')
+
+if ice_cream:
+    st.write('🍦')
+if coffee:
+    st.write('☕')
+if cola:
+    st.write('🥤')
